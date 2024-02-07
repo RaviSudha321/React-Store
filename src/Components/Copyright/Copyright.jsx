69e7a42270db484ab5ca83c09f397563515097ba
Ravi Sudha <ravi321sudha@gmail.com>
2024-02-07 18:48:14 +0530

    Header and Footer created

---
+++ src/Components/Copyright/Copyright.jsx
@@ -0,0 +1,15 @@
+import './Copyright.css';
+
+function Copyright(){
+    return(
+        <div className="copyright_sec">
+            <div className="container">
+                <div className="copyright_text">
+                    <p>2024 All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Copyright;
