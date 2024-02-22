bef882d22753d53440f1e06a17ba4cc913311f28
Ravi Sudha <ravi321sudha@gmail.com>
2024-02-22 13:37:59 +0530

    scroll to top featured added when route change

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { CartContextProvider } from './context/cart.jsx'
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <CartContextProvider>
-        <App />
+        <ScrollToTop />
+          <App />
       </CartContextProvider>
     </BrowserRouter>,
 )
